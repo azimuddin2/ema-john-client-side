ac6a2910b449686cf53ec83be62d2cbed38c3b50
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-01-26 23:58:03 +0600

    added is responsive navbar

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,5 +1,8 @@
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
@@ -8,6 +11,7 @@ import CustomLink from '../CustomLink/CustomLink';
 import './Header.css';
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
     const [user] = useAuthState(auth);
 
     const handleSignOut = () => {
@@ -15,21 +19,41 @@ const Header = () => {
     }
 
     return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div className='menu'>
-                <CustomLink to="/">Home</CustomLink>
-                <CustomLink to="/shop">Shop</CustomLink>
-                <CustomLink to="/order">Order</CustomLink>
-                <CustomLink to="/inventory">Inventory</CustomLink>
-                {
-                    user ?
-                        <button onClick={handleSignOut} className='button'>Logout</button>
-                        :
-                        <Link className='button' to="/login">Login</Link>
-                }
-            </div>
-        </nav>
+        <>
+            <nav className='header'>
+                <img src={logo} alt="" />
+                <div>
+                    <ul id='navbar' className={open ? '#navbar active' : '#navbar'}>
+                        <li>
+                            <CustomLink to="/">Home</CustomLink>
+                        </li>
+                        <li>
+                            <CustomLink to="/shop">Shop</CustomLink>
+                        </li>
+                        <li>
+                            <CustomLink to="/order">Order</CustomLink>
+                        </li>
+                        <li>
+                            <CustomLink to="/inventory">Inventory</CustomLink>
+                        </li>
+                        {
+                            user ?
+                                <button onClick={handleSignOut} className='button'>Logout</button>
+                                :
+                                <Link className='button' to="/login">Login</Link>
+                        }
+                    </ul>
+                </div>
+                <div id='mobile' onClick={() => setOpen(!open)}>
+                    {
+                        open ?
+                            <FontAwesomeIcon className='fa-bars' icon={faTimes}></FontAwesomeIcon>
+                            :
+                            <FontAwesomeIcon className='fa-bars' icon={faBars}></FontAwesomeIcon>
+                    }
+                </div>
+            </nav>
+        </>
     );
 };
 
