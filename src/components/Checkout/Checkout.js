62aac5ef068fd8d4bf05b191fb4060c6431c6e92
azimuddin2 <mohammadazimuddin274@gmail.com>
2023-05-02 23:56:34 +0600

    added a checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/UserContext';
 import './Checkout.css';
 
 const Checkout = () => {
+    const { user } = useContext(AuthContext);
+
     return (
-        <div>
-            <h1>This is Proceed Checkout..!</h1>
+        <div className='form-container'>
+            <form>
+                <div className="input-group">
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder='Your Name'
+                        required
+                    />
+                </div>
+
+                <div className="input-group">
+                    <input
+                        type="text"
+                        name="phone"
+                        placeholder='Your Phone'
+                        required
+                    />
+                </div>
+
+                <div className="input-group">
+                    <input
+                        type="text"
+                        defaultValue={user?.email || ''}
+                        placeholder='Your Name'
+                        disabled
+                    />
+                </div>
+
+                <div className='input-group'>
+                    <select
+                        defaultValue='BDT'
+                        name='currency'
+                        className="input focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                    >
+                        <option value="BDT">BDT</option>
+                        <option value="USD">USD</option>
+                    </select>
+                </div>
+
+                <div className="input-group">
+                    <input
+                        type="text"
+                        placeholder='Your Post Code'
+                        required
+                    />
+                </div>
+
+                <div className="input-group">
+                    <input
+                        type="text"
+                        placeholder='Your Address'
+                        required
+                    />
+                </div>
+                <input className='pay-btn' type="submit" value="PAY" />
+            </form>
         </div>
     );
 };
